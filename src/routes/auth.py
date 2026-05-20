f889a35ea24688c81c767af4767ba1a618da7ea2
Sebastian Resendiz Cardenas <resendizcardenassebastianqgmail.com>
2026-05-20 16:20:12 -0600

    feat: endpoint de login

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -9,9 +9,22 @@ def registro():
     nuevo_usuario = Usuario(
         nombre=data.get('nombre'),
         email=data.get('email'),
-        password=data.get('password'),  # Nota: En producción usar hashing (ej. werkzeug.security)
+        password=data.get('password'), # Text plano temporal por ruta de Frontend
         rol=data.get('rol', 'usuario')
     )
     db.session.add(nuevo_usuario)
     db.session.commit()
     return jsonify({"message": "Usuario creado"}), 201
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    usuario = Usuario.query.filter_by(email=data.get('email'), password=data.get('password')).first()
+    
+    if usuario:
+        return jsonify({
+            "message": "Login exitoso", 
+            "usuario": {"nombre": usuario.nombre, "rol": usuario.rol}
+        }), 200
+    
+    return jsonify({"error": "Credenciales inválidas"}), 401
